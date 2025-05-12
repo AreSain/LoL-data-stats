2625d1d42efa96f990f73d8e52e0d4a94b4df44f
seungsje <seungsje@student.42seoul.kr>
2025-05-12 15:49:39 +0900

    [Feature] Swagger 주석 작성

--- src/main/java/aresain/loldatastats/riot/RiotController.java
+++ src/main/java/aresain/loldatastats/riot/RiotController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -15,6 +17,13 @@ import lombok.RequiredArgsConstructor;
 public class RiotController {
 	private final RiotService riotService;
 
+	@Operation(
+		summary = "Riot 유저 조회 및 Player 생성",
+		description = "Riot API를 이용해 유저를 조회하고, DB에 Player의 puuid를 저장합니다.",
+		responses = {
+			@ApiResponse(responseCode = "201", description = "생성 성공")
+		}
+	)
 	@PostMapping("/account")
 	public ResponseEntity<Void> createAccount(@RequestParam String gameName, @RequestParam String tagLine) {
 		riotService.createAccount(gameName, tagLine);
