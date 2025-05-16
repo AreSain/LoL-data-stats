05bc3bcb232f2e20c25b34467f47b5258371d288
seungsje <seungsje@student.42seoul.kr>
2025-05-17 01:08:41 +0900

    [Docs] 주석 작성 및 간소화

--- src/main/java/aresain/loldatastats/loldata/player/PlayerApi.java
+++ src/main/java/aresain/loldatastats/loldata/player/PlayerApi.java
@@ -3,7 +3,6 @@ package aresain.loldatastats.loldata.player;
 import org.springframework.http.ResponseEntity;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name = "Player", description = "플레이어 관리 API")
@@ -15,21 +14,7 @@ public interface PlayerApi {
             Riot API를 통해 플레이어 정보를 조회하고 DB와 동기화합니다.
             - 플레이어가 DB에 없는 경우: 새로운 플레이어로 생성
             - 플레이어가 DB에 있는 경우: Riot 정보와 비교하여 변경사항이 있을 때만 업데이트
-            """,
-        responses = {
-            @ApiResponse(
-                responseCode = "201",
-                description = "플레이어 정보 동기화 성공"
-            ),
-            @ApiResponse(
-                responseCode = "404",
-                description = "Riot API에서 플레이어를 찾을 수 없음"
-            ),
-            @ApiResponse(
-                responseCode = "500",
-                description = "Riot API 서버 오류 또는 내부 서버 오류"
-            )
-        }
+            """
     )
     ResponseEntity<PlayerDto> syncPlayer(String gameName, String tagLine);
 } 
