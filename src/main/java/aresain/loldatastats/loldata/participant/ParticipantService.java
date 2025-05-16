031ed9c240c89e64b2b18796b3991a4fd216c9a8
seungsje <seungsje@student.42seoul.kr>
2025-05-17 01:19:01 +0900

    [Refactoring] 안쓰는 import 삭제

--- src/main/java/aresain/loldatastats/loldata/participant/ParticipantService.java
+++ src/main/java/aresain/loldatastats/loldata/participant/ParticipantService.java
@@ -2,7 +2,6 @@ package aresain.loldatastats.loldata.participant;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
