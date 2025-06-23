9d90bf283dd4ea669a5de40985361484dd314b82
seungsje <seungsje@student.42seoul.kr>
2025-06-23 17:03:10 +0900

    [Fix] 엔드포인트 전부 쿼리파라미터로 수정

--- src/main/java/aresain/loldatastats/loldata/timeline/TimelineController.java
+++ src/main/java/aresain/loldatastats/loldata/timeline/TimelineController.java
@@ -1,9 +1,9 @@
 package aresain.loldatastats.loldata.timeline;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import aresain.loldatastats.loldata.timeline.dto.TimelineInfoDto;
@@ -15,8 +15,8 @@ import lombok.RequiredArgsConstructor;
 public class TimelineController {
 	private final TimelineService timelineService;
 
-	@PostMapping("/timeline/{matchId}")
-	public ResponseEntity<TimelineInfoDto> saveOrFindTimelineInfo(@PathVariable String matchId) {
+	@PostMapping("/timeline")
+	public ResponseEntity<TimelineInfoDto> saveOrFindTimelineInfo(@RequestParam String matchId) {
 		return ResponseEntity.ok(timelineService.saveOrFindTimelineInfo(matchId));
 	}
 }
