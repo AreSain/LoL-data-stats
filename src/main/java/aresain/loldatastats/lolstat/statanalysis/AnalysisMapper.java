3b63d787b8f8e03d6659aa4f6210b7e0284870ef
seungsje <seungsje@student.42seoul.kr>
2025-06-24 19:56:18 +0900

    [Feature] Analysis 반환 DTO 작성

--- src/main/java/aresain/loldatastats/lolstat/statanalysis/AnalysisMapper.java
+++ src/main/java/aresain/loldatastats/lolstat/statanalysis/AnalysisMapper.java
@@ -1,10 +1,14 @@
 package aresain.loldatastats.lolstat.statanalysis;
 
+import java.util.List;
+
 import org.mapstruct.Mapper;
 
+import aresain.loldatastats.lolstat.statanalysis.dto.AnalysisListDto;
 import aresain.loldatastats.lolstat.statanalysis.dto.ObjectiveAnalysisDto;
 
 @Mapper(componentModel = "spring")
 public interface AnalysisMapper {
 	ObjectiveAnalysisDto toObjectiveAnalysisDto(String type, double winAvgKills, double loseAvgKills);
+	AnalysisListDto toAnalysisListDto(List<ObjectiveAnalysisDto> objectiveAnalysisDtos);
 }
