835be3abea2b91f6c0b6f656574b7d5dfb5a1411
seungsje <seungsje@student.42seoul.kr>
2025-06-25 12:31:22 +0900

    [Refactoring] PlayerMapper Builder 사용하게 수정

--- src/main/java/aresain/loldatastats/loldata/player/PlayerMapper.java
+++ src/main/java/aresain/loldatastats/loldata/player/PlayerMapper.java
@@ -8,12 +8,5 @@ import aresain.loldatastats.riot.dto.AccountDto;
 @Mapper(componentModel = "spring")
 public interface PlayerMapper {
 	PlayerDto toDto(Player entity);
-
-	default Player toEntity(AccountDto accountDto) {
-		return new Player(
-			accountDto.getPuuid(),
-			accountDto.getGameName(),
-			accountDto.getTagLine()
-		);
-	}
+	Player toEntity(AccountDto accountDto);
 }
